1219c170ecae72f3e37554a8134d5effcb9b9e9e
cupofjoy <eelantung@gmail.com>
2020-06-14 15:43:28 -0400

    added spotify functionality

--- src/client/components/LoginBox.js
+++ src/client/components/LoginBox.js
@@ -3,28 +3,32 @@ import "../app.css";
 import spotifyConnect from "../assets/spotifyConnect.png";
 
 const LoginBox = (props) => {
+  const client_id = process.env.REACT_APP_CLIENT_ID;
+  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+  const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&state=34fFs29kd09`;
+
   return (
     <div>
       <div id="login-box">
-        <h1 id="logo">LiveTonight</h1>
+        <h1 id="logo"> LiveTonight </h1>{" "}
         <p>
-          LiveTonight is your one-stop shop for concert listings each and every
-          night. We not only let you know all the bands playing in your city
-          tonight, but we will create a Spotify Playlist of all their top tracks
-          to help you decide which show you don't want to miss!
-        </p>
+          LiveTonight is your one - stop shop for concert listings each and
+          every night.We not only let you know all the bands playing in your
+          city tonight, but we will create a Spotify Playlist of all their top
+          tracks to help you decide which show you don 't want to miss!{" "}
+        </p>{" "}
         <p>
           To get started, click the button below to login to your Spotify
           account and let us take care of the rest!
-        </p>
-        <a href="https://accounts.spotify.com/authorize?client_id=<CLIENT_ID>&redirect_uri=<REDIRECT_URL>&scope=user-read-private%20user-read-email%20user-read-birthdate%20playlist-modify-private%20playlist-modify-public&response_type=token">
+        </p>{" "}
+        <a href={url}>
           <img
             id="spotifyConnectButton"
             src={spotifyConnect}
             alt="Connect to Spotify"
           />
-        </a>
-      </div>
+        </a>{" "}
+      </div>{" "}
     </div>
   );
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,47 @@
 const express = require("express");
+const superagent = require('superagent');
+const dotenv = require('dotenv');
+const path = require('path');
+
+// set up dotenv
+dotenv.config();
 
 const app = express();
 
 app.use(express.static("dist"));
+
+app.get('/callback', (req, res, next) => {
+  // after user logs into spotify, spotify will redirect here
+  // will receive authorization code at req.query.code
+  const body = {
+    grant_type: 'authorization_code',
+    code: req.query.code,
+    redirect_uri: process.env.REDIRECT_URI,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET
+  }
+
+  // SOMEHOW SPOTIFY DIDN'T LIKE FETCH SO HAD TO USE SOMETHING ELSE
+  superagent
+    .post('https://accounts.spotify.com/api/token')
+    .send(body)
+    .set('Content-Type', 'application/x-www-form-urlencoded')
+    .then(data => {
+      console.log('superagent success')
+      const access_token = data.body.access_token;
+      // TODO: SEND ACCESS TOKEN TO FRONT END OR SAVE IT SOMEWHERE
+
+      // REDIRECTS THE USER BACK TO TO REACT HOMEPAGE AFTER SPOTIFY SIGNIN
+      return res.redirect(`http://localhost:3000/`)
+    })
+    .catch(err => {
+      console.log(err)
+      next({
+        err
+      })
+    })
+})
+
+// TO DO: ADD ERROR HANDLING
 
 app.listen(8080, () => console.log("Listening on port 8080!"));
