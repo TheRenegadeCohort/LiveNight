ecfa1439351c651d36ae8f6d916ae10ee2d76763
Steve Budarz <sbudarz@gmail.com>
2020-06-17 08:56:09 -0400

    tests for spotify controller

---
+++ __tests__/server/spotifyController.test.js
@@ -0,0 +1,61 @@
+const supertest = require('supertest');
+const nock = require('nock');
+const spotifyController = require('../../src/server/spotifyController');
+const app = require('../../src/server/app');
+
+const request = supertest(app);
+
+describe('SPOTIFY CONTROLLER', () => {
+  const dummyJSONResponse = {
+    artists: {
+      items: [
+        {
+          id: 'https://api.spotify.com/artist/testResponse',
+        },
+      ],
+    },
+  };
+  //   beforeEach(() => {
+  //     nock('https://api.spotify.com').get('/v1/search');
+  //   });
+
+  it('should send a request to spotify', (done) => {
+    nock('https://api.spotify.com')
+      .get('/v1/search')
+      .query(true)
+      .reply(200, dummyJSONResponse);
+
+    request.get('/artist/testArtist').expect(200, done);
+  });
+
+  it('should send a request to spotify with auth headers', (done) => {
+    nock('https://api.spotify.com')
+      .get('/v1/search')
+      .query(true)
+      .reply(function () {
+        expect(this.req.headers.authorization).toBe('Bearer ');
+        return [200, dummyJSONResponse];
+      });
+
+    request.get('/artist/testArtist').expect(200, done);
+  });
+
+  it('should include querystring in spotify request', (done) => {
+    nock('https://api.spotify.com')
+      .get('/v1/search')
+      .query({
+        type: 'artist',
+        q: 'artist:testArtist',
+        limit: 1,
+      })
+      .reply(200, dummyJSONResponse);
+
+    request.get('/artist/testArtist').expect(200, done);
+  });
+
+  it('should respond with an error if spotify call fails', (done) => {
+    nock('https://api.spotify.com').get('/v1/search').reply(403);
+
+    request.get('/artist/testArtist').expect(400, done);
+  });
+});
