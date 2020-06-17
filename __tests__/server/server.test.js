7a4fe327b5963e19e7fc64cc088052d81dd845f8
Steve Budarz <sbudarz@gmail.com>
2020-06-16 21:40:12 -0400

    basic backend tests

---
+++ __tests__/server/server.test.js
@@ -0,0 +1,53 @@
+const supertest = require('supertest');
+const dotenv = require('dotenv').config();
+const nock = require('nock');
+
+const app = require('../../src/server/app');
+const { response } = require('express');
+
+const request = supertest(app);
+
+describe('sanity check test', () => {
+  it('should hold to the concepts of reality', () => {
+    expect(true).toBeTruthy();
+    expect(true).not.toBeFalsy();
+  });
+});
+
+describe('ENV variables', () => {
+  it('should have defined environmental variables', () => {
+    expect(process.env.REDIRECT_URI).toBeDefined();
+    expect(process.env.CLIENT_ID).toBeDefined();
+    expect(process.env.CLIENT_SECRET).toBeDefined();
+    expect(process.env.TACO_PARTY).toBeUndefined();
+  });
+});
+
+describe('GET /test', () => {
+  it('should respond to a request', (done) => {
+    request.get('/test').expect(200, done);
+  });
+});
+
+describe('GET /callback', () => {
+  const queryCode = { code: 'abc' };
+  beforeEach(() => {
+    nock('https://accounts.spotify.com')
+      .post('/api/token')
+      .reply(200, { access_token: 'abc' });
+  });
+
+  it('should respond to a request with a redirect', (done) => {
+    request
+      .get('/callback')
+      .query(queryCode)
+      .expect('Location', /localhost/g, done);
+  });
+
+  it('should respond to a request with an access token', (done) => {
+    request
+      .get('/callback')
+      .query(queryCode)
+      .expect('Location', /access_token/g, done);
+  });
+});
